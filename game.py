c6101f07598529043458eb57016aa4bfaeb74e6d
meskalito89 <rokinea89mone@mail.ru>
2022-06-09 20:16:30 +0500

    wrote get_random_shape() function

--- game.py
+++ game.py
@@ -1,17 +1,60 @@
 import numpy as np
 from shape import Shape
+import random
 
 
-def create_random_shape(position: tuple[int, int]):
-    variants = (
-        [[-1, 1], [0, 1], [0, -1], [1, -1]],
-        [[-2, 0], [-1, 0], [0, 0], [1, 0]],
-    )
-    random_index = np.random.randint(0, len(variants))
-    return Shape(position, variants[random_index])
+def get_random_shape():
+    variants = {
+        'z_right': [
+            [0, -1],  #   *    
+            [0, 0],  #    **  
+            [1, 0],  #     *  
+            [1, 1]   #        
+        ],
+        'z_left': [
+            [0, -1],  #   *    
+            [0, 0],  #   **   
+            [-1, 0],  #  *     
+            [-1, 1]   #        
+        ],
+        'square': [
+            [0, 0],  #        
+            [1, 0],  #  **    
+            [1, 1],  #  **    
+            [1, 0]   #        
+        ],
+        'l_rigth': [
+            [0, 0],  #  *     
+            [0, 1],  #  *     
+            [0, 2],  #  **    
+            [1, 2]   #        
+        ],
+        'l_left': [
+            [0, 0],  #    *   
+            [0, 1],  #    *   
+            [0, 2],  #   **   
+            [1, 2]   #        
+        ],
+        'T': [
+            [-1, 0],  #  ***   
+            [0, 0],  #    *   
+            [1, 0],  #        
+            [0, 1]   #        
+        ],
+        'I': [
+            [0, -1],  #   *    
+            [0, 0],  #    *   
+            [0, 1],  #    *   
+            [0, 2]   #    *   
+        ],
+    }
+
+    random_shape = random.choice(list(variants.keys()))
+
+    return variants.get(random_shape)
 
 
-class Game:
+class game:
     def __init__(self):
         self.field = np.zeros((20, 10)).astype('bool')
 
@@ -22,11 +65,5 @@ class Game:
                 map(lambda x: simbol_true if x else simbol_false, row)
             )
             field_text += '\n'
-
         return field_text
 
- 
-game = Game()
-print(game.text(0, ' '))
-print(create_random_shape((10, 10)))
-    
